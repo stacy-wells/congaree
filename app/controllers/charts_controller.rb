f726d3c9efbba7b9927d930dfcf3816aa03eb589
Stacy Wells <stacy.wells@americastestkitchen.com>
2017-06-30 14:36:36 -0400

    add index charts and detail page chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,37 @@
 class ChartsController < ApplicationController
   def total_visits
-    render json: Park.group_by_year(:est, format: "%Y").count
+    year_range = 1979..2016
+    totals_by_year = {}
+
+    year_range.each do |year|
+      yearly_totals_array = AnnualVisitorTotal.where(year: Date.new(year)).pluck(:visitors)
+      totals_by_year[year] = yearly_totals_array.reduce(:+)
+    end
+
+    render json: totals_by_year.to_json
+  end
+
+  def park_annual_visitors
+    park = Park.find(params[:id])
+    park_data = park.annual_visitor_totals.pluck(:year, :visitors)
+
+    render json: park_data.to_json
+  end
+
+  def acerage_by_park
+    parks_with_acreage = Park.all.pluck(:name, :acres)
+
+    render json: parks_with_acreage
+  end
+
+  def visitors_by_park
+    total_visitors_by_park = {}
+
+    Park.all.each do |park|
+      visitors = park.annual_visitor_totals.pluck(:visitors).reduce(:+)
+      total_visitors_by_park[park.name] = visitors
+    end
+
+    render json: total_visitors_by_park.to_json
   end
 end
