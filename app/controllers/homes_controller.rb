595ac966f856f5c4a6e01f2cfaf8767c209e9b6f
Stacy Wells <stacy.wells@americastestkitchen.com>
2017-07-05 20:37:30 -0400

    added first round of hero image cuts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
   def home
+    @acres = Park.all.pluck(:acres).reduce(:+).round(2)
+    @visitors = AnnualVisitorTotal.where(year: Date.new(2016)).pluck(:visitors).reduce(:+)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def hero_image_name(park)
+    if park.nil?
+      "hero.png"
+    else
+      "#{park.name.downcase.gsub(' ', '_')}_hero.jpg"
+    end
+  end
 end
